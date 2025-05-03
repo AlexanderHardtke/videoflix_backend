2071c31b83df67abf067f467148c2bc749f00323
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-03 15:29:04 +0200

    general structur and dependencys created

--- up.bat
+++ up.bat
@@ -1,5 +1,5 @@
-@REM git pull
-@REM git add .
-@REM git commit -m "%*"
-@REM git push
-@REM  ssh alucard@34.159.122.31 "cd Coderr && git pull"
+git pull
+git add .
+git commit -m "%*"
+git push
+ ssh alucard@34.159.122.31 "cd Coderr && git pull"
