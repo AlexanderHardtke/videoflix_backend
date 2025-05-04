5e4e5800949ea7127ebec0113fd1bbdb815a6715
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-04 14:15:52 +0200

    admin custom form

--- videoflix_db/admin.py
+++ videoflix_db/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import UserProfil
+from .forms import ProfilUserCreationForm
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+
+@admin.register(UserProfil)
+class UserProfilAdmin(admin.ModelAdmin):
+    add_form = ProfilUserCreationForm
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'Individuelle Daten',
+            {
+                'fields': (
+                    'preferredSize',
+                )
+            }
+        )
+    )
