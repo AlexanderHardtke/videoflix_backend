1cc100d0ad031fef2612643ba2ebdffd0288a638
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-05 07:49:28 +0200

    updated video model

--- videoflix_db/admin.py
+++ videoflix_db/admin.py
@@ -3,20 +3,18 @@ from .models import UserProfil
 from .forms import ProfilUserCreationForm
 from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-
 
 @admin.register(UserProfil)
 class UserProfilAdmin(admin.ModelAdmin):
     add_form = ProfilUserCreationForm
     fieldsets = (
-        *UserAdmin.fieldsets,
         (
             'Individuelle Daten',
             {
                 'fields': (
-                    'preferredSize',
+                    'preferred_size',
                 )
             }
-        )
+        ),
+        *UserAdmin.fieldsets,
     )
--- videoflix_db/models.py
+++ videoflix_db/models.py
@@ -5,11 +5,21 @@ from django.contrib.auth.models import AbstractUser
 
 
 class UserProfil(AbstractUser):
-    preferredSize = models.CharField(max_length=5, default='')
+    preferred_size = models.CharField(max_length=5, default='')
+    sound_volume = models.CharField(max_length=3, default=50)
     
 
 class Video(models.Model):
     created_at = models.DateField(default=date.today)
+    name = models.CharField(max_length=50, default='')
+    image = models.FileField(max_length=99, blank=True, null=True, upload_to='images/')
+    file1080p = models.FileField(max_length=99, blank=True, upload_to='images/')
+    file720p = models.FileField(max_length=99, blank=True, upload_to='images/')
+    file360p = models.FileField(max_length=99, blank=True, upload_to='images/')
+    file120p = models.FileField(max_length=99, blank=True, upload_to='images/')
 
-# class WatchedVideo(models.Model):
-#     One-models.ManyToManyField("app.Model", verbose_name=_(""))
+
+class WatchedVideo(models.Model):
+    user = models.ForeignKey(UserProfil, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    watched_until = models.DurationField()
