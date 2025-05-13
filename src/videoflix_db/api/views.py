f1b48ab268725b7e8929e612e3bdbc41289a4897
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-13 18:16:50 +0200

    updated fileUploadView to include patch and delete

--- src/videoflix_db/api/views.py
+++ src/videoflix_db/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from .serializers import RegistrationSerializer, FileUploadSerializer, VideoSerializer, WatchedVideoSerializer
 from rest_framework.permissions import IsAdminUser
-from videoflix_db.models import UserProfil, Video, WatchedVideo
+from videoflix_db.models import Video, WatchedVideo
 
 
 class RegistrationView(APIView):
@@ -53,11 +53,24 @@ class FileUploadView(APIView):
     permission_classes = [IsAdminUser]
 
     def post(self, request, format=None):
-        serializer = FileUploadSerializer(data=request.date)
+        serializer = FileUploadSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk=None, format=None):
+        file_instance = get_object_or_404(Video, pk=pk)
+        serializer = FileUploadSerializer(file_instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk=None, format=None):
+        file_instance = get_object_or_404(Video, pk=pk)
+        file_instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class VideoView(viewsets.ReadOnlyModelViewSet):
