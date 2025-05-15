3837ce465e15d68787e74ece39982a62af772d35
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-15 14:22:50 +0200

    upload tests done

--- src/videoflix_db/tests/test_watched_video.py
+++ src/videoflix_db/tests/test_watched_video.py
@@ -4,11 +4,14 @@ from rest_framework import status
 from videoflix_db.models import Video, AbstractUser, UserProfil
 
 
-class WatchedVideoTests(APITestCase):
+# class WatchedVideoTests(APITestCase):
 
-    def setUp(self):
-        self.user = AbstractUser.objects.create(
-            username='testuser', password='testpassword'
-        )
-        self.client = APIClient()
-        self.url = reverse('registration-detail')
+#     def setUp(self):
+#         self.admin = create_admin()
+#         self.user = create_user()
+#         self.videos = create_videos(self.admin)
+#         self.video2 = create_videos(self.admin)
+#         self.client = APIClient()
+#         self.token = Token.objects.create(user=self.admin)
+#         self.url_list = reverse('video-list')
+#         self.url = reverse('video-detail')
