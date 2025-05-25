bb90f6eac5afaba5704cc64289f037d485864131
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-25 16:34:38 +0200

    Send email out if passwort is forgotten

--- src/videoflix_db/api/views.py
+++ src/videoflix_db/api/views.py
@@ -9,10 +9,9 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.exceptions import UnsupportedMediaType
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from videoflix_db.models import Video, WatchedVideo
+from videoflix_db.models import Video, WatchedVideo, PasswordForgetToken, EmailConfirmationToken, UserProfil
 from .serializers import RegistrationSerializer, FileUploadSerializer, VideoSerializer, WatchedVideoSerializer, VideoListSerializer
 from .permissions import IsEmailConfirmed
-from videoflix_db.models import EmailConfirmationToken
 import secrets
 import requests
 
@@ -27,76 +26,86 @@ class RegistrationView(APIView):
 
         if not serializer.is_valid():
             return Response(
-                {"error": serializer.errors},
+                {'error': serializer.errors},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
         saved_user = serializer.save()
         token = secrets.token_hex(32)
         EmailConfirmationToken.objects.create(user=saved_user, token=token)
-        lang = request.data.get("lang", "de")
-        url = "https://videoflix.alexander-hardtke.com/confirm-email-link-de.php" if lang == "de" else \
-              "https://videoflix.alexander-hardtke.com/confirm-email-link-en.php"
+        lang = request.data.get('lang', 'de')
+        url = 'https://videoflix.alexander-hardtke.com/confirm-email-link-de.php' if lang == 'de' else \
+              'https://videoflix.alexander-hardtke.com/confirm-email-link-en.php'
 
         try:
             requests.post(
                 url,
                 json={
-                    "email": saved_user.email,
-                    "token": token,
+                    'email': saved_user.email,
+                    'token': token,
                 },
-                headers={"Content-Type": "application/json"},
+                headers={'Content-Type': 'application/json'},
                 timeout=5
             )
         except requests.RequestException as error:
-            return Response({"registration": error}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        return Response({"registration": "Confirm your email address"}, status=status.HTTP_201_CREATED)
-    
+            return Response({'registration': error}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'registration': 'Confirm your email address'}, status=status.HTTP_201_CREATED)
+
 
 class ConfirmEmailView(APIView):
     def post(self, request):
         token = request.data.get('token')
         if not token:
-            return Response({"error": "Token is not valid or expired"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Token is not valid or expired'}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            confirmation_token = EmailConfirmationToken.objects.get(token=token)
+            confirmation_token = EmailConfirmationToken.objects.get(
+                token=token)
         except EmailConfirmationToken.DoesNotExist:
-            return Response({"error": "Token is not valid or expired"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Token is not valid or expired'}, status=status.HTTP_400_BAD_REQUEST)
 
         if confirmation_token.is_expired():
             confirmation_token.delete()
-            return Response({"error": "Token is not valid or expired"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Token is not valid or expired'}, status=status.HTTP_400_BAD_REQUEST)
 
         user = confirmation_token.user
         user.email_confirmed = True
         user.save()
         confirmation_token.delete()
 
-        return Response({"success": "email address confirmed"}, status=status.HTTP_200_OK)
-    
+        return Response({'success': 'email address confirmed'}, status=status.HTTP_200_OK)
+
 
 class ResetPasswordView(APIView):
     def post(self, request):
-        token = request.data.get('token')
-        if not token:
-            return Response({"error": "Token fehlt"}, status=status.HTTP_400_BAD_REQUEST)
+        lang = request.data.get('lang', 'de')
+        email = request.data.get('email')
+        if not email:
+            return Response({'error': 'Email is missing'}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            confirmation_token = EmailConfirmationToken.objects.get(token=token)
-        except EmailConfirmationToken.DoesNotExist:
-            return Response({"error": "Ungültiger Token"}, status=status.HTTP_400_BAD_REQUEST)
+            saved_user = UserProfil.objects.get(email=email)
+        except UserProfil.DoesNotExist:
+            return Response({'reset': 'Check your email to reset password'}, status=status.HTTP_201_CREATED)
 
-        if confirmation_token.is_expired():
-            confirmation_token.delete()
-            return Response({"error": "Token abgelaufen"}, status=status.HTTP_400_BAD_REQUEST)
+        token = secrets.token_hex(32)
+        PasswordForgetToken.objects.create(user=saved_user, token=token)
+        url = 'https://videoflix.alexander-hardtke.com/send-reset-link-de.php' if lang == 'de' else \
+              'https://videoflix.alexander-hardtke.com/send-reset-link-en.php'
 
-        user = confirmation_token.user
-        user.email_confirmed = True
-        user.save()
-        confirmation_token.delete()
-
-        return Response({"success": "E-Mail bestätigt"}, status=status.HTTP_200_OK)
+        try:
+            requests.post(
+                url,
+                json={
+                    'email': saved_user.email,
+                    'token': token,
+                },
+                headers={'Content-Type': 'application/json'},
+                timeout=5
+            )
+        except requests.RequestException as error:
+            return Response({'reset': error}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'reset': 'Check your email to reset password'}, status=status.HTTP_201_CREATED)
 
 
 class LoginView(ObtainAuthToken):
@@ -166,7 +175,7 @@ class VideoView(viewsets.ReadOnlyModelViewSet):
 
     def retrieve(self, request, *args, **kwargs):
         video_id = kwargs['pk']
-        cache_key = f"video_meta:{video_id}"
+        cache_key = f'video_meta:{video_id}'
 
         meta = cache.get(cache_key)
         if meta is None:
@@ -177,7 +186,7 @@ class VideoView(viewsets.ReadOnlyModelViewSet):
         watched_video, _ = WatchedVideo.objects.get_or_create(
             user=request.user,
             video_id=video_id,
-            defaults={"watched_until": 0}
+            defaults={'watched_until': 0}
         )
 
         meta_with_progress = meta.copy()
