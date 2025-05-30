d78c9490b638924cca3f45f601fe55c5ea4c5d54
Alexander Hardtke <5yl3kz@googlemail.com>
2025-05-30 08:08:42 +0200

    fixed registration tests

--- src/videoflix_db/tests/test_registration_login.py
+++ src/videoflix_db/tests/test_registration_login.py
@@ -2,6 +2,8 @@ from django.urls import reverse
 from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 from .test_data import create_user, create_incative_user
+from unittest.mock import patch
+from django.conf import settings
 
 
 class CreateUserTests(APITestCase):
@@ -9,6 +11,10 @@ class CreateUserTests(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.url = reverse('registration-detail')
+        settings.EMAIL_CONFIRM_URL = "https://mocked-url.com"
+        patcher = patch('videoflix_db.api.views.requests.post')
+        self.mock_post = patcher.start()
+        self.addCleanup(patcher.stop)
 
     def test_create_user(self):
         data = {
@@ -18,54 +24,56 @@ class CreateUserTests(APITestCase):
         }
         response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.mock_post.assert_called_once()
         self.assertEqual(
             response.data["registration"], "Confirm your email address")
 
-#     def test_duplicate_user(self):
-#         data = {
-#             'email': 'example@mail.de',
-#             'password': 'examplePassword',
-#             'repeated_password': 'examplePassword'
-#         }
-#         response = self.client.post(self.url, data, format='json')
-#         response_duplicate = self.client.post(self.url, data, format='json')
-#         self.assertEqual(response_duplicate.status_code,
-#                          status.HTTP_201_CREATED)
-#         self.assertEqual(
-#             response_duplicate.data["registration"], "Confirm your email address")
+    def test_duplicate_user(self):
+        data = {
+            'email': 'example@mail.de',
+            'password': 'examplePassword',
+            'repeated_password': 'examplePassword'
+        }
+        response = self.client.post(self.url, data, format='json')
+        response_duplicate = self.client.post(self.url, data, format='json')
+        self.assertEqual(response_duplicate.status_code,
+                         status.HTTP_201_CREATED)
+        self.assertEqual(
+            response_duplicate.data["registration"], "Confirm your email address")
+        self.assertEqual(self.mock_post.call_count, 2)
 
-#     def test_fail_password_create_user(self):
-#         data = {
-#             'email': 'example@mail.de',
-#             'password': 'rightPassword',
-#             'repeated_password': 'wrongPassword'
-#         }
-#         response = self.client.post(self.url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_fail_password_create_user(self):
+        data = {
+            'email': 'example@mail.de',
+            'password': 'rightPassword',
+            'repeated_password': 'wrongPassword'
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-#     def test_fail_email_create_user(self):
-#         data = {
-#             'email': 'noEmail',
-#             'password': 'rightPassword',
-#             'repeated_password': 'rightPassword'
-#         }
-#         response = self.client.post(self.url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_fail_email_create_user(self):
+        data = {
+            'email': 'noEmail',
+            'password': 'rightPassword',
+            'repeated_password': 'rightPassword'
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-#     def test_invalid_method_create_user(self):
-#         response = self.client.get(self.url)
-#         self.assertEqual(
-#             response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED
-#         )
-#         data = {'email': 'exampleUsername'}
-#         response = self.client.patch(self.url, data, format='json')
-#         self.assertEqual(
-#             response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED
-#         )
-#         response = self.client.delete(self.url)
-#         self.assertEqual(
-#             response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED
-#         )
+    def test_invalid_method_create_user(self):
+        response = self.client.get(self.url)
+        self.assertEqual(
+            response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+        data = {'email': 'exampleUsername'}
+        response = self.client.patch(self.url, data, format='json')
+        self.assertEqual(
+            response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+        response = self.client.delete(self.url)
+        self.assertEqual(
+            response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED
+        )
 
 
 # class UserLoginTests(APITestCase):
@@ -154,3 +162,13 @@ class CreateUserTests(APITestCase):
 #         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 # Registration Token and Password reset
+
+# class EmailConfirmationTests(APITestCase):
+
+#     def test_valid_token_confirms_email(self):
+#         # 1. Token manuell anlegen
+#         # 2. API-Aufruf zur Bestätigung simulieren
+#         # 3. assert: User.email_confirmed = True
+
+#     def test_invalid_token_returns_400(self):
+#         ...
