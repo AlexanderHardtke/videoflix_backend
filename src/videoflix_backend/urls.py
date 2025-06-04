47fbffa373def568c156f13e5c189b3904408470
Alexander Hardtke <5yl3kz@googlemail.com>
2025-06-04 10:06:56 +0200

    login function for staff added

--- src/videoflix_backend/urls.py
+++ src/videoflix_backend/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('videoflix_db.api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('django-rq/', include('django_rq.urls'))
 ]
 
