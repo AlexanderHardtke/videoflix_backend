35ef3a7e45373151bdc6c9466e8366c12a4df874
Alexander Hardtke <5yl3kz@googlemail.com>
2025-07-06 08:08:57 +0200

    token tests done

--- videoflix_db/tests/test_token.py
+++ videoflix_db/tests/test_token.py
@@ -1,116 +1,195 @@
-# from django.urls import reverse
 # from rest_framework.test import APIClient, APITestCase
+# from rest_framework import status
+# from django.utils import timezone
+# from django.urls import reverse
+# from django.contrib.auth import get_user_model
+# from .test_data import create_user
+# from authemail.models import SignupCode, PasswordResetCode
+# from datetime import timedelta
+# from videoflix_db.models import UserProfil
+
+
+# User = get_user_model()
 
 
 # class ActivateAccountTests(APITestCase):
 #     def setUp(self):
 #         self.client = APIClient()
-#         self.url = reverse('registration-detail')
+#         self.email = 'example@mail.de'
 #         data = {
-#             'email': 'example@mail.de',
+#             'email': self.email,
 #             'password': 'examplePassword',
 #             'repeated_password': 'examplePassword'
 #         }
-#         self.client.post(self.url, data, format='json')
-#         self.token = EmailConfirmationToken.objects.get(user__email='example@mail.de').token
+#         url = reverse('registration-detail')
+#         self.client.post(url, data, format='json')
+#         self.url = reverse('confirm-detail')
 
 #     def test_activate_account(self):
-#         url = reverse('confirm-detail')
-#         response = self.client.post(url, {'token': self.token}, format='json')
-#         self.assertEqual(response.status_code, 200)
+#         token = SignupCode.objects.filter(user__email=self.email).first()
+#         response = self.client.get(self.url + f'?code={token.code}')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
 #         self.assertEqual(response.data['success'], 'Email confirmed')
-#         user = UserProfil.objects.get(email='example@mail.de')
-#         self.assertTrue(user.email_confirmed)
+#         user = User.objects.get(email=self.email)
+#         self.assertTrue(user.is_verified)
 
 #     def test_invalid_token_activate_account(self):
-#         url = reverse('confirm-detail')
-#         response = self.client.post(url, {'token': 'invalid'}, format='json')
-#         self.assertEqual(response.status_code, 400)
+#         response = self.client.get(self.url + f'?code={'invalid'}')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 #         self.assertEqual(response.data['error'], 'Token is not valid or expired')
-#         user = UserProfil.objects.get(email='example@mail.de')
-#         self.assertFalse(user.email_confirmed)
+#         user = User.objects.get(email=self.email)
+#         self.assertFalse(user.is_verified)
 
 #     def test_no_token_activate_account(self):
-#         url = reverse('confirm-detail')
-#         response = self.client.post(url, format='json')
-#         self.assertEqual(response.status_code, 400)
+#         response = self.client.get(self.url)
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 #         self.assertEqual(response.data['error'], 'Token is not valid or expired')
-#         user = UserProfil.objects.get(email='example@mail.de')
-#         self.assertFalse(user.email_confirmed)
+#         user = User.objects.get(email=self.email)
+#         self.assertFalse(user.is_verified)
 
 #     def test_expired_token_activate_account(self):
-#         user = UserProfil.objects.get(email='example@mail.de')
-#         expired_time = timezone.now() - timedelta(days=2)
-#         token = EmailConfirmationToken.objects.create(
-#             user=user,
-#             token='expired-token-123'
-#         )
-#         token.created_at = expired_time
+#         token = SignupCode.objects.get(user__email=self.email)
+#         token.created_at = timezone.now() - timedelta(days=2)
 #         token.save()
-#         url = reverse('confirm-detail')
-#         response = self.client.post(url, {'token': token.token}, format='json')
-#         self.assertEqual(response.status_code, 400)
+#         response = self.client.get(self.url + f'?code={token.code}')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 #         self.assertEqual(response.data['error'], 'Token is not valid or expired')
-#         self.assertFalse(user.email_confirmed)
+#         user = User.objects.get(email=self.email)
+#         self.assertFalse(user.is_verified)
 
 #     def test_invalid_method_activate_account(self):
-#         url = reverse('confirm-detail')
-#         response = self.client.get(url)
+#         token = SignupCode.objects.filter(user__email=self.email).first()
+#         response = self.client.post(self.url + f'?code={token.code}')
 #         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-#         response = self.client.patch(url, {'token': self.token}, format='json')
+#         response = self.client.patch(self.url + f'?code={token.code}')
 #         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-#         response = self.client.delete(url)
+#         response = self.client.delete(self.url + f'?code={token.code}')
 #         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-# class RefreshTokenTests(APITestCase):
+# class PasswordSendResetTests(APITestCase):
 
 #     def setUp(self):
-#         self.user = create_user()
 #         self.client = APIClient()
-#         self.login_url = reverse('login-detail')
-#         self.refresh_url = reverse('refresh-detail') # Assuming 'refresh-detail' for refresh endpoint
+#         self.user = create_user()
+#         self.url = reverse('reset-detail')
 
-#     def test_refresh_token_success(self):
-#         login_data = {
-#             'email': 'example@mail.de',
-#             'password': 'examplePassword'
+#     def test_create_reset_password(self):
+#         data = {'email': 'example@mail.de'}
+#         response = self.client.post(self.url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#         self.token = PasswordResetCode.objects.filter(user__email='example@mail.de').first()
+#         self.assertIsNotNone(self.token)
+
+#     def test_wrong_email_create_reset_password(self):
+#         data = {'email': 'wrong@mail.de'}
+#         response = self.client.post(self.url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#         token_exists = PasswordResetCode.objects.filter(user__email='wrong@mail.de').exists()
+#         self.assertFalse(token_exists, 'No token for wrong email')
+
+#     def test_invalid_method_create_reset_password(self):
+#         response = self.client.get(self.url)
+#         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+#         data = {'email': 'example@mail.de'}
+#         response = self.client.patch(self.url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+#         response = self.client.delete(self.url)
+#         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+# class PasswordChangeTests(APITestCase):
+
+#     def setUp(self):
+#         self.client = APIClient()
+#         self.user = create_user()
+#         url = reverse('reset-detail')
+#         data = {'email': 'example@mail.de'}
+#         self.client.post(url, data, format='json')
+#         self.token = PasswordResetCode.objects.filter(user__email='example@mail.de').first()
+#         self.url = reverse('verify-detail')
+#         self.change_url = reverse('change-detail')
+
+#     def test_change_password(self):
+#         verify_response = self.client.get(self.url + f'?code={self.token.code}')
+#         self.assertEqual(verify_response.status_code, status.HTTP_200_OK)
+#         data = {
+#             'password': 'newPassword',
+#             'repeatPw': 'newPassword',
+#             'code': self.token.code
 #         }
-#         self.client.post(self.login_url, login_data, format='json')
-
-#         response = self.client.post(self.refresh_url)
+#         response = self.client.post(self.change_url, data, format='json')
 #         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertIn('access', response.cookies)
-#         self.assertIn('refresh', response.cookies)
-#         self.assertTrue(response.cookies['access']['httponly'])
-#         self.assertTrue(response.cookies['refresh']['httponly'])
+#         self.assertEqual(response.data['success'], 'Password changed successfully')
+#         user = UserProfil.objects.get(email='example@mail.de')
+#         self.assertTrue(user.check_password('newPassword'))
 
-#     def test_refresh_token_without_refresh_cookie(self):
-#         response = self.client.post(self.refresh_url)
-#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-#     def test_refresh_token_with_invalid_refresh_cookie(self):
-#         login_data = {
-#             'email': 'example@mail.de',
-#             'password': 'examplePassword'
+#     def test_invalid_token_change_password(self):
+#         verify_response = self.client.get(self.url + f'?code={self.token.code}')
+#         self.assertEqual(verify_response.status_code, status.HTTP_200_OK)
+#         data = {
+#             'password': 'newPassword',
+#             'repeatPw': 'newPassword',
+#             'code': 'invalidToken'
 #         }
-#         self.client.post(self.login_url, login_data, format='json')
+#         response = self.client.post(self.change_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+#         self.assertEqual(response.data['error'], 'Token is not valid or expired')
 
-#         self.client.cookies['refresh'] = 'invalid_refresh_token'
-#         response = self.client.post(self.refresh_url)
-#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-#     def test_refresh_token_with_expired_refresh_cookie(self):
-#         login_data = {
-#             'email': 'example@mail.de',
-#             'password': 'examplePassword'
+#     def test_no_token_change_password(self):
+#         verify_response = self.client.get(self.url + f'?code={''}')
+#         self.assertEqual(verify_response.status_code, status.HTTP_400_BAD_REQUEST)
+#         data = {
+#             'password': 'newPassword',
+#             'repeated_password': 'newPassword'
 #         }
-#         self.client.post(self.login_url, login_data, format='json')
+#         response = self.client.post(self.change_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+#         self.assertEqual(response.data['error'], 'Token is not valid or expired')
 
-#         # Manually create an expired refresh token
-#         refresh = RefreshToken.for_user(self.user)
-#         refresh.set_exp(lifetime=timedelta(seconds=-1)) # Expire immediately
-#         self.client.cookies['refresh'] = str(refresh)
+#     def test_no_password_change_password(self):
+#         verify_response = self.client.get(self.url + f'?code={self.token.code}')
+#         self.assertEqual(verify_response.status_code, status.HTTP_200_OK)
+#         data = {
+#             'password': '',
+#             'repeated_password': '',
+#             'code': self.token.code
+#         }
+#         response = self.client.post(self.change_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+#         self.assertEqual(response.data['error'], 'Password is missing')
 
-#         response = self.client.post(self.refresh_url)
-#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+#     def test_expired_token_change_password(self):
+#         token = PasswordResetCode.objects.get(user__email='example@mail.de')
+#         token.created_at = timezone.now() - timedelta(days=4)
+#         token.save()
+#         verify_url = reverse('verify-detail') + f'?code={token.code}'
+#         verify_response = self.client.get(verify_url)
+#         self.assertEqual(verify_response.status_code, status.HTTP_400_BAD_REQUEST)
+#         data = {
+#             'password': 'newPassword',
+#             'repeatPw': 'newPassword',
+#             'code': self.token.code
+#         }
+#         response = self.client.post(self.change_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+#         self.assertEqual(response.data['error'], 'Token is not valid or expired')
+
+#     def test_invalid_method_change_password(self):
+#         verify_response = self.client.post(self.url + f'?code={self.token.code}')
+#         self.assertEqual(verify_response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+#         verify_response = self.client.patch(self.url + f'?code={self.token.code}')
+#         self.assertEqual(verify_response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+#         verify_response = self.client.delete(self.url + f'?code={self.token.code}')
+#         self.assertEqual(verify_response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+#         data = {
+#             'password': 'newPassword',
+#             'repeatPw': 'newPassword',
+#             'code': self.token.code
+#         }
+#         response = self.client.get(self.change_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+#         response = self.client.patch(self.change_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+#         response = self.client.delete(self.change_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
