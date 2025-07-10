0a986cd88c4c11fcc1544f8ca7e361617eb400d5
Alexander Hardtke <5yl3kz@googlemail.com>
2025-07-10 08:33:29 +0200

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -163,7 +163,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'videoflix_db.auth.CookieJWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
 }
